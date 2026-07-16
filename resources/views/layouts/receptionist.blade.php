89501ea8c8e82ca73f771f4580ff702fc03c9554
Daaaaaav <scarletstormsubs@gmail.com>
2026-07-16 18:18:43 +0700

    fixes for the blades

--- resources/views/layouts/receptionist.blade.php
+++ resources/views/layouts/receptionist.blade.php
@@ -173,8 +173,8 @@ $invertStyle = 'filter: brightness(0) invert(1);';
             </header>
 
             {{-- Main Scrollable Area --}}
-            <main class="flex-1 overflow-y-auto overflow-x-hidden relative animate-fade-in-up transition-[padding] duration-300 ease-in-out pb-2 lg:pb-0" :style="isMobile ? 'padding-left: 0;' : (sidebarLocked ? 'padding-left: 280px;' : 'padding-left: 64px;')">
-                <div class="w-full h-full px-4 sm:px-6 lg:px-8 py-4 lg:py-0
+            <main class="flex-1 overflow-y-auto overflow-x-hidden relative animate-fade-in-up transition-[padding] duration-300 ease-in-out" :style="isMobile ? 'padding-left: 0;' : (sidebarLocked ? 'padding-left: 280px;' : 'padding-left: 64px;')">
+                <div class="w-full min-h-full px-4 sm:px-6 lg:px-8 py-4 lg:py-0 pb-24 lg:pb-4
                             [&_.container]:max-w-none [&_.container]:mx-0 [&_.container]:px-0">
 
                 {{-- Premium Top Header Bar --}}
